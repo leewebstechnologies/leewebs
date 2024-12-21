75815f2238e510f422605bbb3ba6cb6981d27232
Ali <aliuajibade317@gmail.com>
2024-12-21 09:18:22 +0100

    -complete navbar

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Leewebs</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/styles.css">
+  </head>
+  <body>
+    <!-- NAVBAR -->
+     <nav class="navbar navbar-expand-lg bg-dark">
+      <div class="container">
+        <a href="#" class="navbar-brand">Leewebs</a>
+        <div class="collapse navbar-collapse" id="navbarcollapseLeewebs">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a href="./myprofile.php" class="nav-link"><i class="fa-solid fa-user text-success"></i> My Profile</a>
+            </li>
+            <li class="nav-item">
+              <a href="dashboard.php" class="nav-link">Dashboard</a>
+            </li>
+            <li class="nav-item">
+              <a href="posts.php" class="nav-link">Posts</a>
+            </li>
+            <li class="nav-item">
+              <a href="categories.php" class="nav-link">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a href="admin.php" class="nav-link">Manage Admins</a>
+            </li>
+            <li class="nav-item">
+              <a href="comments.php" class="nav-link">Comments</a>
+            </li>
+            <li class="nav-item">
+              <a href="blog.php?page=1" class="nav-link">Live Blog</a>
+            </li>
+          </ul>        
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="./logout.php" class="navlink"><i class="fa-solid fa-user-xmark text-danger"></i> Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+     </nav>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  </body>
+</html>
