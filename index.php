57a2f6165e298af353617bc2c723373b020b1604
Ali <aliuajibade317@gmail.com>
2024-12-31 23:06:16 +0100

    add navbar

--- index.php
+++ index.php
@@ -9,11 +9,15 @@
     <link rel="stylesheet" href="./css/styles.css">
   </head>
   <body>
-    <!-- NAVBAR -->
-     <nav class="navbar navbar-expand-lg bg-dark">
+    <div style="height: 10px; background-color: #27aae1;"></div>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a href="#" class="navbar-brand">Leewebs</a>
-        <div class="collapse navbar-collapse" id="navbarcollapseLeewebs">
+        <!-- <img class="logo" src="./images/logo.png" alt="Leewebs Logo"> -->
+        <a href="/" class="navbar-brand">Leewebs</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
               <a href="./myprofile.php" class="nav-link"><i class="fa-solid fa-user text-success"></i> My Profile</a>
@@ -45,9 +49,41 @@
         </div>
       </div>
      </nav>
-   
+    <div style="height: 10px; background-color: #27aae1;"></div>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <p>Hello</p>
+    <!-- NAVBAR ENDS -->
+     <footer class="bg-dark text-white">
+      <div class="container">
+        <div class="row">
+          <div class="">
+             <p class="lead text-center">Theme By | Leewebs Technologies | &copy; --- All Rights Reserved</p>
+          </div>        
+        </div>
+      </div>
+     </footer>
+     <script src="./js/scripts.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
   </body>
 </html>
